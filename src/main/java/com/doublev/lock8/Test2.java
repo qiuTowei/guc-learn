8336363b2948f8ec2fa78c660556fc23ef50803e
doubleVV <qiuwei1991@yeah.net>
2021-03-06 15:42:45 +0800

    new push

---
+++ src/main/java/com/doublev/lock8/Test2.java
@@ -0,0 +1,15 @@
+package com.doublev.lock8;
+
+/**
+ * @ Project: juc
+ * @ Package: com.doublev.lock8
+ * @ Title 标题（要求能简洁地表达出类的功能和职责）
+ * @ Description: 描述（简要描述类的职责、实现方式、使用注意事项等）
+ * @ author : qw
+ * @ CreateDate: 2021/3/5 13:25
+ * @ Version: 1.0
+ * @ Copyright: Copyright (c) 2021
+ * @ History: 修订历史（历次修订内容、修订人、修订时间等）
+ */
+public class Test2 {
+}
